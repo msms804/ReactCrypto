b9c49a2ba82e11969d8470d939a0cc2498c4e09d
msms804 <104570214+msms804@users.noreply.github.com>
2024-02-25 23:40:00 +0900

    코인 api로 불러오기

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import axios from 'axios';
+//https://startatage30.tistory.com/29
+//위에거를 일단 클론코딩하자. 코인종목 옆에 가격 실시간
+//cors 이슈 해결해야
+function App() {
+  const [data, setData] = useState(null);//
+  /**
+   * 
+     useEffect(() => {
+      const timer = setTimeout(async () => {
+        try {
+          const res = await fetch("https://api.upbit.com/v1/market/all", {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: { accept: "application/json" }
+          });
+          const json = await res.json();
+          console.log(json);
+        } catch (err) {
+          console.log(err);
+        }
+      }, 0);
+  
+      return () => {
+        clearTimeout(timer);
+      };
+    }, []);
+  useEffect(() => {
+    fetch("https://api.upbit.com/v1/market/all?isDetails=true", {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => { response.json() })
+      .catch(error => console.error('Error:', error));
+  }, []);
+   */
+
+
+  useEffect(() => {
+    axios.get("https://api.upbit.com/v1/market/all?isDetails=true")
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+  useEffect(() => {
+    console.log("data:", data);
+  }, [data])
+
+  return (
+    <>
+      {/* 데이터를 화면에 표시하는 코드 */}
+      괄호문제는 왜 안되는지 꼭확인할것
+      syntax error input~
+      SyntaxError: Unexpected end of input
+    </>
+  )
+}
+
+export default App
