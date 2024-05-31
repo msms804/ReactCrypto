54af4fc0cebf5c54e38cb5d404d4ed0763a17b45
msms804 <104570214+msms804@users.noreply.github.com>
2024-05-31 23:44:32 +0900

    서버와 연결후 db에서 코인목록 받기

--- src/pages/Theme.tsx
+++ src/pages/Theme.tsx
@@ -1,6 +1,30 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+interface Coins {
+    name: string,
+    ticker: string,
+}
 
 export const Theme = () => {
+    const [coins, setCoins] = useState<Coins[]>([]);
+    useEffect(() => {
+        const fetchCoins = async () => {
+            try {
+                const result = await axios.get('http://localhost:8080/api/coins')
+                console.log("서버량 연결테스트", result.data);
+                setCoins(result.data);
+            } catch (error) {
+                console.log("서버 연결 실패", error);
+            }
+        }
+        fetchCoins();
+    }, [])
     return (
-        <div>Theme</div>
+        <div>
+            Theme
+            <div>
+                {coins.map((item) => (item.name))}
+            </div>
+        </div>
     )
 }
