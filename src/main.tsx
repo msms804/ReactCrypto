756102b53956fa02721538e40c83ee31a080171d
msms804 <104570214+msms804@users.noreply.github.com>
2024-06-07 16:43:41 +0900

    react-query-devtools 설치후 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <BrowserRouter>
         <App />
       </BrowserRouter>
