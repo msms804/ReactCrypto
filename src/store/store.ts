a4e1d4e21a827f5b448ee22c395324b38fb3fa86
msms804 <104570214+msms804@users.noreply.github.com>
2024-06-10 16:27:36 +0900

    실시간 업비트 가격 redux store에 저장

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,68 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
+import counterReducer, { counterSlice } from './upbitSlice'
 
+interface UpbitCoin {
+
+    koreanname: string,
+    englishname: string,
+    theme: string,
+    ticker: string,
+    shortname: string,
+    image: string,
+    cryptoExchange: string,
+    trade_price: number,//가격
+    acc_trade_price_24h: number,//거래대금
+    signed_change_rate: number, //등락폭
+    change: string,
+}
+interface UpbitState {
+    coins: UpbitCoin[];
+}
+const initialState: UpbitState = {
+    coins: [],
+}
 //useState역할
-// let upbitcoins = createSlice({
-//     name: "upbitcoins",
-//     initialState: "값",
-// })
-export default configureStore({
-    reducer: {
-        // upbitcoins: upbitcoins.reducer,
+const upbitCoinSlice = createSlice({
+    name: 'upbitcoins',
+    initialState,
+    reducers: {
+        setUpbit: (state, action) => {//state는 위의 initial state말함
+            state.coins = action.payload;
+        },
+        updateUpbitPrice: (state, action) => {
+            const { upbitticker, upbitTradePrice, upbitacc, upbitchangerate, upbitchange } = action.payload;
+            //state.coins.find((action.payload) => action.payload)
+            const coin = state.coins.find(coin => coin.ticker === upbitticker);
+            if (coin) {
+                coin.trade_price = upbitTradePrice;
+                coin.acc_trade_price_24h = upbitacc;
+                coin.signed_change_rate = upbitchangerate;
+                coin.change = upbitchange;
+            }
+        }
     }
-}) 
+})
+//액션 및 리듀서 내보내기
+export const { setUpbit, updateUpbitPrice } = upbitCoinSlice.actions;
+export const upbitReducers = upbitCoinSlice.reducer;
+
+export const store = configureStore({
+    reducer: {
+        //upbitcoins: upbitcoinsReducer,
+        // counter: counterSlice,
+        upbit: upbitReducers,
+        //upbitTradePrice:  
+    }
+})
+/**
+ * 아오 머리아파
+ * 1. 일단 리액트쿼리로 디비에서 받아왔음
+ * 2. 그 데이터 store에 저장? 
+ * 3. 소켓으로 받을때마다 가격 갱신
+ * 
+ */
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
