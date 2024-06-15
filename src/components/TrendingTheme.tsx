10ed3372d7827d4ab3a048ce56575a7c791cf83e
msms804 <104570214+msms804@users.noreply.github.com>
2024-06-16 01:19:46 +0900

    상승률 높은 테마 top3 구현

--- src/components/TrendingTheme.tsx
+++ src/components/TrendingTheme.tsx
@@ -1,24 +1,86 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useUpbitThemes from '../hooks/useUpbitThemes'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
+import { IUpbitThemes, IUpbitThemeCoins } from '../typings/db'
 
+interface UpbitCoins {
+    ticker: string,
+    image: string,
+    shortname: string,
+    signed_change_rate: number,
+    change: string,
+}
+interface UpbitThemes {
+    theme: string,
+    name: string,
+    description: string,
+    avg_change_rate: number,
+    coins: UpbitCoins[],
+}
 export const TrendingTheme = () => {
-    const mappedThemes = useSelector((state: RootState) => state.theme.mappedThemes)
+    const [upbitTheme, setUpbitTheme] = useState<UpbitThemes[]>();
+    const mappedThemes = useSelector((state: RootState) => state.theme.mappedThemes as IUpbitThemes[])
 
     useUpbitThemes();
+
     useEffect(() => {
         console.log("킁킁", mappedThemes)
+
+        const mappedUpbitThemes: UpbitThemes[] = mappedThemes.map((item: any) => {
+            const avgChangeRate = calculateAvgChangeRate(item.coins);
+            console.log("킁킁킁", calculateAvgChangeRate(item.coins));
+            return {
+                ...item,
+                avg_change_rate: avgChangeRate,
+            }
+        }).sort((a, b) => b.avg_change_rate - a.avg_change_rate);
+        console.log("머여", mappedUpbitThemes);
+        setUpbitTheme(mappedUpbitThemes.slice(0, 3));
     }, [mappedThemes])
 
+    const calculateAvgChangeRate = (coins: IUpbitThemeCoins[]) => {//item.coins를 넘겨줌
+        var initialValue = 0;
+
+        var sum = coins.reduce(
+            (accumulator: any, currentValue: any) => accumulator + currentValue.signed_change_rate,
+            initialValue,
+        )
+        return (sum * 100) / coins.length;
+    }
+
+    // useEffect(() => { console.log("머임 ㅡㅡ", upbitTheme) }, [upbitTheme])
+
     return (
-        <div className='h-full flex flex-col '>
-            {
+        <div className='h-full flex flex-col  space-y-2'>
+            {/* {
                 mappedThemes.slice(0, 3).map((item: any) => (
                     <div className='flex-1 bg-slate-100 rounded-xl'>
                         {item.name}
                     </div>))
+            } */}
+            {
+                upbitTheme?.map((item, idx) => (
+                    <div key={idx} className='flex flex-1 bg-slate-100 rounded-lg justify-between'>
+                        <div className='flex flex-row space-x-4 items-center ml-2'>{/* items-center 추가 */}
+                            <span className='font-medium'>{idx + 1}</span>
+                            <div className='flex -space-x-3 overflow-hidden w-20'>
+                                {
+                                    item.coins.slice(0, 3).map((coin, idx) =>
+                                        <img className="w-7 h-7 rounded-full inline-block ring-1 ring-blue-200 bg-white" src={coin.image} />
+                                    )
+                                }
+                            </div>
+                            <span className='font-medium text-sm'>{item.name}</span>
+                        </div>
+                        <div className='flex items-center'> {/* items-center 추가 */}
+                            <span className='text-red-500 font-medium mr-2 text-sm'>{item.avg_change_rate.toFixed(2)}%</span>
+
+                        </div>
+                    </div>
+                ))
             }
+
         </div>
     )
 }
