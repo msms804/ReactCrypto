f9d69ffa42ea676357ecffc64c6cb916f44b7202
msms804 <104570214+msms804@users.noreply.github.com>
2024-06-29 02:59:42 +0900

    마우스 호버 했을때 즐겨찾기 리스트 나타나도록 디자인

--- src/components/WatchListModal.tsx
+++ src/components/WatchListModal.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const WatchListModal = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    }
+
     return (
-        <div>WatchListModal</div>
+        <div className='relative'>
+            <button
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}>
+                WatchList
+            </button>
+            {
+                isHovered
+                && <div
+                    className='absolute top-10 left-0 w-64 bg-white border-gray shadow-lg rounded p-4'
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}>
+                    <ul>
+                        <li>BTC     $60,852.56      -1.58% </li>
+                        <li>ETH     $60,852.56      -2.08% </li>
+                        <li>SOL     $60,852.56      -3.92% </li>
+                    </ul>
+                </div>
+            }
+        </div>
+
     )
 }
