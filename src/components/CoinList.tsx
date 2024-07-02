75a84a0e4ca7e032376776d60376b41bf4a3b36a
msms804 <104570214+msms804@users.noreply.github.com>
2024-07-02 22:03:51 +0900

    관심종목 로컬스토리지에 저장하는 로직 구현

--- src/components/CoinList.tsx
+++ src/components/CoinList.tsx
@@ -64,6 +64,7 @@ const CoinList = () => {
     const [inputValue, setInputValue] = useState('');
     const [filteredKrwCoins, setFilteredKrwCoins] = useState<upbit[]>([]);
     const [searchedCoins, setSearchedCoins] = useState<upbit[]>([]);
+    const [watchList, setWatchList] = useState<string[]>([]);
     useUpbitWebsocket();
 
 
@@ -145,6 +146,33 @@ const CoinList = () => {
         }
     }, [selectedTheme, searchedCoins])
 
+    useEffect(() => {
+        //1. 일단 로컬스토리지에서 watchlist 가져온다,getItem
+        //2. 만약 없으면 setItem으로 빈배열 집어넣기
+        const watched = localStorage.getItem('watchlist');
+        if (watched) {
+            setWatchList(JSON.parse(watched));
+        } else {
+            localStorage.setItem('watchlist', JSON.stringify([]));
+        }
+    }, [])
+    const onClickWatched = (ticker: string) => {
+        console.log('누른코인의 티커는?', ticker)
+        setWatchList((prev) => {
+            if (!prev.some(item => item === ticker)) {
+                const newList = [...prev, ticker];
+                localStorage.setItem('watchlist', JSON.stringify(newList));
+                return newList;
+            } else {
+                const newList = prev.filter(item => item !== ticker);
+                localStorage.setItem('watchlist', JSON.stringify(newList))
+                return newList
+            }
+        })
+    }
+    useEffect(() => {
+        console.log("와치리스트 state", watchList)
+    }, [watchList])
 
     const saveInDB = async (e: any) => {
         e.preventDefault();
@@ -240,10 +268,15 @@ const CoinList = () => {
                         {selectedCurrency === "KRW" ?
                             filteredKrwCoins?.slice(0, 20).map((item, index) => (
                                 <tr key={index}>
-                                    <td>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 w-3 h-3">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                        </svg>
+                                    <td onClick={() => { onClickWatched(item.shortname) }}>
+                                        {watchList.includes(item.shortname)
+                                            ? <svg xmlns="http://www.w3.org/2000/svg" fill="orange" viewBox="0 0 24 24" strokeWidth="1.5" stroke="none" className="size-6 w-4 h-4">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                            </svg>
+                                            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 w-3 h-3">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                            </svg>
+                                        }
                                     </td>
                                     <td className="py-2 px-4 text-sm text-left">{index + 1}</td>
                                     <td className="flex flex-row items-center space-x-2">
@@ -262,7 +295,7 @@ const CoinList = () => {
                             )) :
                             reduxUsdtCoins.slice(0, 20).map((item, index) => (
                                 <tr key={index}>
-                                    <td>
+                                    <td onClick={() => { onClickWatched(item.shortname) }}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 w-3 h-3">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                                         </svg>
