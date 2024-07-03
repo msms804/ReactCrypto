5944cf95f0f86b950c98014b044ead9468209a74
msms804 <104570214+msms804@users.noreply.github.com>
2024-07-04 00:57:13 +0900

    리덕스에 저장된 관심코인목록 , 실시간가격 매핑후 렌더링 구현

--- src/components/WatchListModal.tsx
+++ src/components/WatchListModal.tsx
@@ -1,8 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { RootState } from '../store/store';
+import { useSelector } from 'react-redux';
+import useUpbitWebsocket from '../hooks/useUpbitWebsocket';
+import { IUpbit } from '../typings/db';
 
 export const WatchListModal = () => {
     const [isHovered, setIsHovered] = useState(false);
+    const [watchList, setWatchList] = useState<string[]>([]);
+    const watchlist = useSelector((state: RootState) => state.watchlist.items)
+    const reduxUpbitCoins = useSelector((state: RootState) => state.upbit.coins)
+    const [upbitWatched, setUpbitWatched] = useState<IUpbit[]>([]);
+    // useUpbitWebsocket(); //이게 꼭 필요한가?
 
+    useEffect(() => {
+        // console.log("리덕스 테스트", reduxUpbitCoins)
+        if (reduxUpbitCoins && watchlist) {
+            //1. redux중 watchlist에 해당하는거만 추출
+            //2. 이미지, 퍼센테이지, 가격을 upbitWatched에 저장
+            //3. 렌더링
+            // watchlist.filter(item => reduxUpbitCoins.includes(item))
+            const newWatched = reduxUpbitCoins.filter(item => watchlist.includes(item.ticker))
+            setUpbitWatched(newWatched);
+        }
+    }, [watchlist, reduxUpbitCoins])
+    useEffect(() => {
+        console.log(",,", upbitWatched)
+    }, [upbitWatched])
     const handleMouseEnter = () => {
         setIsHovered(true);
     }
@@ -10,6 +33,32 @@ export const WatchListModal = () => {
     const handleMouseLeave = () => {
         setIsHovered(false);
     }
+    //https://tailwindcss.com/docs/position 
+    //position 공부할것
+    const loadWatchList = () => {
+        const watched = localStorage.getItem('watchlist')
+        if (watched !== null) {
+            console.log("모달창에서 가져옴", watched);
+            console.log("모달창에서 가져옴2", JSON.parse(watched));
+            setWatchList(JSON.parse(watched))
+        }
+
+    }
+    useEffect(() => {
+        loadWatchList();
+
+        const handleStorageChange = (event: StorageEvent) => {
+            if (event.key === 'watchlist') {
+                // console.log()
+                loadWatchList();
+            }
+        }
+        window.addEventListener('storage', handleStorageChange);
+
+        return () => {
+            window.removeEventListener('storage', handleStorageChange);
+        }
+    }, [])
 
     return (
         <div className='relative'>
@@ -21,14 +70,37 @@ export const WatchListModal = () => {
             {
                 isHovered
                 && <div
-                    className='absolute top-10 left-0 w-64 bg-white border-gray shadow-lg rounded p-4'
+                    className='absolute top-10 left-1/2 transform -translate-x-1/2 w-80 h-96 overflow-x-auto rounded-lg bg-white border-gray shadow-lg rounded p-4'
                     onMouseEnter={handleMouseEnter}
                     onMouseLeave={handleMouseLeave}>
-                    <ul>
-                        <li>BTC     $60,852.56      -1.58% </li>
-                        <li>ETH     $60,852.56      -2.08% </li>
-                        <li>SOL     $60,852.56      -3.92% </li>
-                    </ul>
+                    <h2 className='text-lg font-semibold mb-4'>관심종목</h2>
+                    <table className='w-full'>
+                        <thead>
+                            <tr>
+                                <th className='text-xs text-gray-500 text-left border-b border-y-gray-200 p-1'>코인명</th>
+                                <th className='text-xs text-gray-500 text-right border-b border-y-gray-200 p-1'>가격</th>
+                                <th className='text-xs text-gray-500 text-right border-b border-y-gray-200 p-1'>등락폭(24h)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {upbitWatched.map(item =>
+                                <tr key={item.ticker}>
+                                    <td className='p-1'>
+                                        <div className='flex flex-row items-center space-x-2'>
+                                            <img src={item.image} className='w-5 h-5' />
+                                            <div className='flex flex-col space-y-1'>
+                                                <div className='text-xs font-medium'>{item.ticker.split('-')[1]} / {item.ticker.split('-')[0]}</div>
+                                                <div className='text-xs text-gray-500'>{item.koreanname}</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className={`text-right p-1 ${item.signed_change_rate >= 0 ? 'text-red-400' : 'text-blue-500'}`}>{item.trade_price}</td>
+                                    <td className={`text-right p-1 ${item.signed_change_rate >= 0 ? 'text-red-400' : 'text-blue-500'}`}>{(item.signed_change_rate * 100).toFixed(2)}%</td>
+                                </tr>
+                            )}
+
+                        </tbody>
+                    </table>
                 </div>
             }
         </div>
