57eb29e8cd2b9b962266824735fa0d80c873d7b3
msms804 <104570214+msms804@users.noreply.github.com>
2024-07-07 01:30:15 +0900

    코인 분류 css 변경

--- src/components/CoinList.tsx
+++ src/components/CoinList.tsx
@@ -5,11 +5,12 @@ import { SevenDays } from "./SevenDays";
 import useUpbitCoins from "../queries/upbitcoins";
 import { Search } from "./Search";
 import { useDispatch, useSelector } from "react-redux";
-import { setUpbit, updateUpbitPrice } from "../store/store";
+import { AppDispatch, setUpbit, updateUpbitPrice } from "../store/store";
 import useUpbitWebsocket from "../hooks/useUpbitWebsocket";
 import { RootState } from '../store/store'
 import { Cointable } from "./Cointable";
 import { IUpbitThemes } from '../typings/db';
+import { setWatchlist, addToWatchlist, removeFromWatchlist } from "../store/watchlistSlice";
 
 
 interface Market {
@@ -54,7 +55,7 @@ const CoinList = () => {
     const [updatedUpbitCoins, setUpdatedUpbitCoins] = useState<upbit[]>([]);
     const [usdtUpbitCoins, setUsdtUpbitCoins] = useState<upbit[]>([])//updatedUpbitCoins중 usdt만 필터링한것
     const [krwUpbitCoins, setKrwUpbitCoins] = useState<upbit[]>([])//updatedUpbitCoins중 krw만 필터링한것
-    const dispatch = useDispatch();
+    //const dispatch = useDispatch();
     const reduxUpbitCoins = useSelector((state: RootState) => state.upbit.coins)
     const [reduxKrwCoins, setReduxKrwCoins] = useState<upbit[]>([]);
     const [reduxUsdtCoins, setReduxUsdtCoins] = useState<upbit[]>([])
@@ -65,6 +66,8 @@ const CoinList = () => {
     const [filteredKrwCoins, setFilteredKrwCoins] = useState<upbit[]>([]);
     const [searchedCoins, setSearchedCoins] = useState<upbit[]>([]);
     const [watchList, setWatchList] = useState<string[]>([]);
+    const dispatch: AppDispatch = useDispatch();
+    const watchlist = useSelector((state: RootState) => state.watchlist.items);
     useUpbitWebsocket();
 
 
@@ -151,24 +154,34 @@ const CoinList = () => {
         //2. 만약 없으면 setItem으로 빈배열 집어넣기
         const watched = localStorage.getItem('watchlist');
         if (watched) {
-            setWatchList(JSON.parse(watched));
+            // setWatchList(JSON.parse(watched));
+            dispatch(setWatchlist(JSON.parse(watched)));
         } else {
             localStorage.setItem('watchlist', JSON.stringify([]));
         }
     }, [])
     const onClickWatched = (ticker: string) => {
         console.log('누른코인의 티커는?', ticker)
-        setWatchList((prev) => {
-            if (!prev.some(item => item === ticker)) {
-                const newList = [...prev, ticker];
-                localStorage.setItem('watchlist', JSON.stringify(newList));
-                return newList;
-            } else {
-                const newList = prev.filter(item => item !== ticker);
-                localStorage.setItem('watchlist', JSON.stringify(newList))
-                return newList
-            }
-        })
+        // setWatchList((prev) => {
+        //     if (!prev.some(item => item === ticker)) {
+        //         const newList = [...prev, ticker];
+        //         localStorage.setItem('watchlist', JSON.stringify(newList));
+        //         return newList;
+        //     } else {
+        //         const newList = prev.filter(item => item !== ticker);
+        //         localStorage.setItem('watchlist', JSON.stringify(newList))
+        //         return newList
+        //     }
+        // })
+        if (!watchlist.includes(ticker)) {//로컬스토리지, store에 추가
+            dispatch(addToWatchlist(ticker))
+            localStorage.setItem('watchlist', JSON.stringify([...watchlist, ticker]));
+            console.log("디스패치 추가", ticker);
+        } else {//로컬스토리지, store에 삭제
+            dispatch(removeFromWatchlist(ticker))
+            localStorage.setItem('watchlist', JSON.stringify(watchlist.filter(item => item !== ticker)));
+            console.log("디스패치 삭제", ticker)
+        }
     }
     useEffect(() => {
         console.log("와치리스트 state", watchList)
@@ -214,23 +227,24 @@ const CoinList = () => {
     if (upbitLoading) return <div>upbit loading...</div>
     return (<>
         <div className="container mx-auto mt-8">
-            <div className="flex flex-row space-x-2 justify-between m-4">{/**왜 이렇게 밑에 넣어야함;;; */}
-                <div className=" flex flex-row bg-slate-100 rounded-full p-1 space-x-2">
-                    <div className={`rounded-full p-2  text-sm ${selectedCurrency === "KRW" ? 'bg-[#30d5c8] text-black' : "bg-white"}`}
-                        onClick={() => { setSelectedCurrency("KRW") }}>원화</div>
-                    <div className={`rounded-full p-2 text-sm ${selectedCurrency === "USDT" ? 'bg-[#30d5c8] text-white' : 'bg-white'}`}
-                        onClick={() => { setSelectedCurrency("USDT") }}>USDT</div>
-                </div>
-            </div>
-            <div className="flex flex-row justify-between">
-                <div className='flex flex-row space-x-2 m-4'>
 
-                    {selectedCurrency === "KRW" && themes?.map((item) =>
-                        <button
-                            onClick={() => { handleThemeChange(item) }}
-                            key={item.theme}
-                            className={`p-1 text-xs text-slate-500 border-b border-slate-200 rounded-lg ${selectedTheme?.theme === item.theme ? 'bg-[#30d5c8] text-black' : ''}`}
-                        >{item.name}</button>)}
+            <div className="flex flex-row justify-between">
+                <div className="flex flex-row space-x-2 m-4">{/**왜 이렇게 밑에 넣어야함;;; */}
+                    <div className=" flex flex-row space-x-2">
+                        <div className={`border rounded-full px-2 py-1 text-xs ${selectedCurrency === "KRW" ? 'text-[#30d5c8] border-[#30d5c8]-400' : "text-gray-400 border-gray-400"}`}
+                            onClick={() => { setSelectedCurrency("KRW") }}>원화</div>
+                        <div className={`border rounded-full px-2 py-1 text-xs ${selectedCurrency === "USDT" ? 'text-[#30d5c8] border-[#30d5c8]-400' : "text-gray-500 border-gray-400"}`}
+                            onClick={() => { setSelectedCurrency("USDT") }}>USDT</div>
+                    </div>
+                    <div className="border-l border-gray-400"></div>
+                    <div className='flex flex-row space-x-2'>
+                        {selectedCurrency === "KRW" && themes?.map((item) =>
+                            <button
+                                onClick={() => { handleThemeChange(item) }}
+                                key={item.theme}
+                                className={`px-2 py-1 text-xs border rounded-full ${selectedTheme?.theme === item.theme ? 'border-[#30d5c8]-400 text-[#30d5c8]' : 'border-slate-200 text-gray-500'}`}
+                            >{item.name}</button>)}
+                    </div>
                 </div>
                 <div className="relative">
 
@@ -268,12 +282,12 @@ const CoinList = () => {
                         {selectedCurrency === "KRW" ?
                             filteredKrwCoins?.slice(0, 20).map((item, index) => (
                                 <tr key={index}>
-                                    <td onClick={() => { onClickWatched(item.shortname) }}>
-                                        {watchList.includes(item.shortname)
-                                            ? <svg xmlns="http://www.w3.org/2000/svg" fill="orange" viewBox="0 0 24 24" strokeWidth="1.5" stroke="none" className="size-6 w-4 h-4">
+                                    <td onClick={() => { onClickWatched(item.ticker) }} className="flex justify-center items-center">
+                                        {watchlist.includes(item.ticker)
+                                            ? <svg xmlns="http://www.w3.org/2000/svg" fill="orange" viewBox="0 0 24 24" strokeWidth="1.5" stroke="orange" className="size-6 w-4 h-4">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                                             </svg>
-                                            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 w-3 h-3">
+                                            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="gray" className="size-6 w-4 h-4">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                                             </svg>
                                         }
@@ -295,10 +309,15 @@ const CoinList = () => {
                             )) :
                             reduxUsdtCoins.slice(0, 20).map((item, index) => (
                                 <tr key={index}>
-                                    <td onClick={() => { onClickWatched(item.shortname) }}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 w-3 h-3">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                        </svg>
+                                    <td onClick={() => { onClickWatched(item.ticker) }}>
+                                        {watchlist.includes(item.ticker)
+                                            ? <svg xmlns="http://www.w3.org/2000/svg" fill="orange" viewBox="0 0 24 24" strokeWidth="1.5" stroke="orange" className="size-6 w-4 h-4">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                            </svg>
+                                            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="gray" className="size-6 w-4 h-4">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                            </svg>
+                                        }
                                     </td>
                                     <td className="py-2 px-4">{index + 1}</td>
                                     <td className="flex flex-row items-center space-x-2">
