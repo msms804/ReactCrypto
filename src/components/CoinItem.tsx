4abeae2d230f1687ffa92a7a9211595ed8ff4f02
msms804 <104570214+msms804@users.noreply.github.com>
2024-09-26 02:42:21 +0900

    coinitem 컴포넌트로 따로 분류

---
+++ src/components/CoinItem.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { SevenDays } from './SevenDays'
+interface CoinItemProps {
+    onClickWatched: (ticker: string) => void;
+    item: {
+        ticker: string;
+        shortname: string;
+        koreanname: string;
+        image: string;
+        trade_price: number;
+        signed_change_rate: number;
+        acc_trade_price_24h: number;
+        change: string;
+    };
+    index: number;
+    watchlist: string[];
+}
+export const CoinItem: React.FC<CoinItemProps> = ({ onClickWatched, item, index, watchlist }) => {
+    return (
+        <tr>
+            <td onClick={() => { onClickWatched(item.ticker) }} className="flex justify-center items-center">
+                {watchlist.includes(item.ticker)
+                    ? <svg xmlns="http://www.w3.org/2000/svg" fill="orange" viewBox="0 0 24 24" strokeWidth="1.5" stroke="orange" className="size-6 w-4 h-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                    </svg>
+                    : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="gray" className="size-6 w-4 h-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                    </svg>
+                }
+            </td>
+            <td className="py-2 px-4 text-sm text-left">{index + 1}</td>
+            <td className="flex flex-row items-center space-x-2">
+                <span><img src={item.image} className="w-7 h-7 rounded-full" /></span>
+                <span className="font-medium">{item.shortname}</span>
+                <span className="text-xs text-gray-500 ml-2">{item.koreanname}</span>
+
+            </td>
+            <td className={`${item.change === "RISE" ? "text-red-500" : "text-blue-600"} font-light`}>{item.trade_price?.toLocaleString('ko-KR')}</td>
+            <td className={`${item.change === "RISE" ? "text-red-500" : "text-blue-600"} font-light`}>{(item.signed_change_rate * 100).toFixed(2)}%</td>
+            <td className="font-light"> {item.acc_trade_price_24h?.toLocaleString('ko-KR')}</td>
+            <td className="text-right"><SevenDays ticker={item.ticker} change={item.change} /></td>
+        </tr>
+    )
+}
