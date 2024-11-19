58e34dbad75e19ff376328ae7d2e3796388e5025
msms804 <104570214+msms804@users.noreply.github.com>
2024-11-20 03:28:53 +0900

    gcTime , staleTime 적용하여 30분마다 갱신

--- src/hooks/useUpbitThemes.ts
+++ src/hooks/useUpbitThemes.ts
@@ -1,16 +1,19 @@
-import { IUpbitThemes, IUpbitThemeCoins } from "../typings/db";
+import { IUpbitThemes } from "../typings/db";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import useUpbitCoins from "../queries/upbitcoins";
 import axios from "axios";
 import { setUpbitThemes } from "../store/upbitThemeSlice";
-
+import useThemeCoinsPrice from "../queries/themecoinsPrice";
+import { useQuery } from "@tanstack/react-query";
 //여기서 테마별 평균도 넣어야
 const useUpbitThemes = () => {
     const dispatch = useDispatch();
-    const { data: upbitcoins, error: upbitError, isLoading: upbitLoading } = useUpbitCoins();
+    const { data: upbitcoins, isLoading: upbitLoading } = useUpbitCoins();
     const [themes, setThemes] = useState<IUpbitThemes[]>();
     const [mappedUpbitThemes, setMappedUpbitThemes] = useState<IUpbitThemes[]>();
+    //const { data: themecoins, error: coinsError, isLoading: coinsLoading } = useThemeCoinsPrice();
+    const { data: themecoins } = useThemeCoinsPrice();
 
     //db에서 코인별 theme 가져옴
     useEffect(() => {
@@ -46,11 +49,42 @@ const useUpbitThemes = () => {
             })
 
             setMappedUpbitThemes(mappedTheme);
-            //--> 이게아니라 바로 리덕스에 저장해야
-            // console.log(",.", mappedTheme);
             dispatch(setUpbitThemes(mappedTheme))
         }
     }, [upbitcoins, themes, dispatch])
+
+    useEffect(() => {
+        // themecoins.nextUpdate > 현재시각 --> 갱신할 필요 x
+
+        // themecoins.nextUpdate < 현재시각 --> 갱신해야
+        //const now = dayjs();
+        // if (!themecoins.nextUpdate || dayjs(themecoins.nextUpdate).isAfter(now)) {
+        // 업비트 api 요청
+
+        //db에 새로 update
+
+        // invalidate Queries
+        // }
+
+    }, [themecoins])
+    // 코인 가격을 가져오는 useQuery 훅(업비트 API 요청)
+    const { data: priceData, isLoading: priceLoading } = useQuery({
+        queryKey: ["upbitPrices", mappedUpbitThemes],
+        queryFn: async () => {
+            const tickers = mappedUpbitThemes?.flatMap((theme) => theme.coins.map(coin => coin.ticker));
+            const uniqueTickers = [...new Set(tickers)].join(',');
+            if (uniqueTickers) {
+                const response = await axios.get(`https://api.upbit.com/v1/ticker?markets=${uniqueTickers}`)
+                return response.data;
+            }
+            return [];
+        },
+        enabled: !!mappedUpbitThemes,//데이터가 있을때만 요청
+        staleTime: 1000 * 60 * 30, //30분(staleTime을 30분으로 설정)
+        gcTime: 1000 * 60 * 60, //1시간(30분 이상으로 설정하여 캐시보관)
+        refetchOnWindowFocus: false,  // 포커스 변경 시 리패치 방지
+    })
+
 
     //코인가격 매핑
     useEffect(() => {
@@ -58,12 +92,12 @@ const useUpbitThemes = () => {
             const tickers = mappedUpbitThemes?.flatMap((theme) => theme.coins.map(coin => coin.ticker));
             const uniqueTickers = [...new Set(tickers)].join(',');
 
-            if (uniqueTickers) {
+            if (priceData && uniqueTickers) {
                 try {
-                    const response = await axios.get(`https://api.upbit.com/v1/ticker?markets=${uniqueTickers}`)
+                    // const response = await axios.get(`https://api.upbit.com/v1/ticker?markets=${uniqueTickers}`)
                     const finalThemes = mappedUpbitThemes?.map((theme: any) => {
                         const coinlist = theme.coins.map((ticker: any) => {
-                            const result = response.data.find((item: any) => item.market === ticker.ticker)
+                            const result = priceData.find((item: any) => item.market === ticker.ticker)
                             console.log("종가", result.trade_price)
                             return {
                                 ticker: ticker.ticker,
@@ -84,7 +118,7 @@ const useUpbitThemes = () => {
                     if (finalThemes) {
                         setMappedUpbitThemes(finalThemes);
                         //여기서 dispatch
-                        console.log(",.", finalThemes);//여기서 에러남
+                        console.log("무한루프", finalThemes);//여기서 에러남
                         dispatch(setUpbitThemes(finalThemes));
                     }
                 } catch (error) {
@@ -95,9 +129,12 @@ const useUpbitThemes = () => {
         if (mappedUpbitThemes) {
             fetchUpbitPrice();
         }
-    }, [mappedUpbitThemes, dispatch])
+    }, [priceData, dispatch])
 
-
+    return {//이게 머임..?
+        themes: mappedUpbitThemes,
+        isLoading: upbitLoading || priceLoading, //로딩상태병합
+    }
 
 }
 export default useUpbitThemes;
